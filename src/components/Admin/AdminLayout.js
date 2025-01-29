56808f182cd4f9778ea597ab79775e0ec349a76e
someswar <someswar@klizos.com>
2025-01-29 19:28:30 +0530

    updated bugs

--- src/components/Admin/AdminLayout.js
+++ src/components/Admin/AdminLayout.js
@@ -1,15 +1,29 @@
 import { Box } from "@mui/material";
 import Topbar from "./Topbar";
 import Sidebar from "./Sidebar";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import Cookies from "js-cookie";
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import styles from '../../styles/BasicDetails.module.css'
 
 export const AdminLayout = () => {
+
+  const token = Cookies.get("accessToken");
+  
+  const reset=()=>{
+    window.scrollTo(0,0)
+  }
+  if (!token) return <Navigate to="/" />;
+
   return (
     <Box display="flex">
       <Sidebar />
       <Box flex={1} bgcolor="#f7f9fc" >
         <Topbar />
-        <Box p={2}><Outlet/></Box>
+        <Box p={2}>
+          <Outlet/>
+          <ArrowUpwardIcon className={styles.arrow} onClick={reset}/>
+        </Box>
       </Box>
     </Box>
   );
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,25 +1,25 @@
 import Cookies from "js-cookie";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthNavbar } from "./AuthNavbar";
-import {
-  Box,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import { Search } from "./Search";
-
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import styles from "../styles/BasicDetails.module.css";
 
 export const ProtectedRoute = () => {
   const token = Cookies.get("accessToken");
-  console.log(token);
 
-  
-
+  const reset = () => {
+    window.scrollTo(0, 0);
+  };
   if (!token) return <Navigate to="/" />;
 
   return (
     <Box>
       <AuthNavbar />
-      <Search/>
+      <Search />
       <Outlet />
+      <ArrowUpwardIcon className={styles.arrow} onClick={reset} />
     </Box>
   );
 };
